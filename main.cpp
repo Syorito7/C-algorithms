50bcc4b9f36d00bf5d2bb4b9cdf9b5520fa47ac3
Никита Индюченко <101006635+Syorito7@users.noreply.github.com>
2025-11-20 16:44:08 +0300

    обновлённый main_2

--- main.cpp
+++ main.cpp
@@ -2,23 +2,35 @@
 #include <locale>
 #include <windows.h>
 #include <ctime>           // Добавлено для работы с датой
+#include "string_tasks.h" // не работает подключение
 #include "arrays_tasks.h"
-#include "string_tasks.h"
+#include "Matrix.h" // не работает подключение
 
 
 void print_info() {
-    std::cout << "Доступные команды:\n";
-    std::cout << "0: help      - Показать это сообщение\n";
-    std::cout << "1: arrays    - Работа с задачами по массивам\n";
-    std::cout << "2: strings   - Работа со строками\n";
-    std::cout << "3: matrix    - Работа с двухмерными массивами\n";
-    std::cout << "4: sort      - Сортировки\n";
-    std::cout << "5: files     - Работа с файлами\n";
-    std::cout << "6: pointers  - Работа с указателями\n";
-    std::cout << "7: dynamic   - Работа с динамической памятью\n";
-    std::cout << "8: lists     - Задачи на односвязные списки\n";
-    std::cout << "-1: exit     - Выйти из программы\n";
+    std::cout << "\nДоступные команды:\n";
+    std::cout << "----------------------------------------------------\n";
+    std::cout << "  0  | help      - Показать это сообщение\n";
+    std::cout << "  1  | arrays    - Работа с задачами по массивам\n";
+    std::cout << "  2  | strings   - Работа со строками\n";
+    std::cout << "  3  | matrix    - Работа с двухмерными массивами\n";
+    std::cout << "  4  | sort      - Сортировки\n";
+    std::cout << "  5  | files     - Работа с файлами\n";
+    std::cout << "  6  | pointers  - Работа с указателями\n";
+    std::cout << "  7  | dynamic   - Работа с динамической памятью\n";
+    std::cout << "  8  | lists     - Задачи на односвязные списки\n";
+    std::cout << "  9  | dlists    - Работа с двусвязными списками\n";
+    std::cout << " 10  | trees     - Работа с деревьями\n";
+    std::cout << " 11  | hashtab   - Работа с хеш-таблицами\n";
+    std::cout << " 12  | graphs    - Работа с графами\n";
+    std::cout << " 13  | sets      - Множества\n";
+    std::cout << " -1  | exit      - Выйти из программы\n";
+    std::cout << "----------------------------------------------------\n";
 }
+
+void inputMatrix(int arr[][Matrix::MAX], int& rows, int& cols);
+
+
 
 int main() {
     // Устанавливаем русскую локаль
@@ -44,8 +56,16 @@ int main() {
             string_tasks::string_menu();
             break;
         case 3:
-            // работа с двухмерными массивами
+        {
+			// работа с матрицами
+            int rows = 0, cols = 0;
+            int matrix[Matrix::MAX][Matrix::MAX];
+
+            inputMatrix(matrix, rows, cols);
+            if (rows > 0 && cols > 0)
+                Matrix::menu(matrix, rows, cols);
             break;
+        }
         case 4:
             // сортировки
             break;
@@ -98,4 +118,30 @@ int main() {
     }
 
     return 0;
+}
+
+
+void inputMatrix(int arr[][Matrix::MAX], int& rows, int& cols) {
+    std::cout << "Введите количество строк (не более " << Matrix::MAX << "): ";
+    std::cin >> rows;
+    if (rows <= 0 || rows > Matrix::MAX) {
+        std::cout << "Неверное количество строк!\n";
+        rows = 0;
+        cols = 0;
+        return;
+    }
+
+    std::cout << "Введите количество столбцов (не более " << Matrix::MAX << "): ";
+    std::cin >> cols;
+    if (cols <= 0 || cols > Matrix::MAX) {
+        std::cout << "Неверное количество столбцов!\n";
+        rows = 0;
+        cols = 0;
+        return;
+    }
+
+    std::cout << "Введите элементы матрицы построчно:\n";
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < cols; ++j)
+            std::cin >> arr[i][j];
 }
