33e65c02dac591a7aef4cb60448e0c30806bd699
Никита Индюченко <101006635+Syorito7@users.noreply.github.com>
2025-11-21 18:20:09 +0300

    Добавление задачек на указатели

---
+++ pointers.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+namespace Tasks_Pointers {
+
+	// ======================== Функции для задач ========================
+	void increment(int* ptr);
+	void swap(int* x, int* y);
+
+	// ======================== Решения задач ============================
+	void task1();
+	void task2();
+	void task3();
+	void task4();
+
+	// ======================== Меню для вызова задач =====================
+	void menu();
+
+} // namespace Tasks
+#pragma once
